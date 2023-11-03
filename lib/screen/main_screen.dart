acfbba88c8071d41e68185396bb6cc5d99050026
deliciousBanana <tts4419@gmail.com>
2023-11-03 18:52:19 +0900

    Idea Note 11/03

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:idea_note_app/database/database_helper.dart';
+
+import '../data/idea_info.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,10 +12,117 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  var dbHelper = DatabaseHelper(); // 데이터 베이스 접근을 용이하게 해주는 유틸 객체
+  List<IdeaInfo> listIdeaInfo = []; // 아이디어 목록들
+
+  @override
+  void initState() { //위젯이 생성될때 처음으로 호출되는 메서드 이다. initState는 오직 한번 만 호출 된다.
+    super.initState();
+    //아이디어 목록들 가져오기
+    getIdeaInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Idea Note',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return listItem(index);
+          },
+        ),
+      ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: () {
+         ///새 아이디어 작성 화면으로 이동
+       },
+       child: Icon(Icons.add)
+     ),
     );
+  }
+
+  Widget listItem(int index) {
+    //Stack 쌓아올리다 겹칠수 잇다 햄버거 처럼
+    return Container(
+      height: 65,
+      margin: EdgeInsets.only(top: 15),
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+        side: BorderSide(color: Color(0xffd9d9d9), width: 2),
+        borderRadius: BorderRadius.circular(10),
+      )),
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          Align(
+            /// 아이디어 제목
+            alignment: Alignment.topLeft,
+            child: Container(
+              margin: EdgeInsets.only(top: 8, left: 15),
+              child: Text(
+                'Exam Text',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+          Align(
+
+              ///기록 날짜
+              alignment: Alignment.bottomRight,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 8, right: 15),
+                child: Text(
+                  '2023.11.03 17:42',
+                  style: TextStyle(
+                    color: Color(0xffaeaeae),
+                    fontSize: 10,
+                  ),
+                ),
+              )),
+          Align(
+            /// 중요도
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 8, left: 15),
+              child: RatingBar.builder(
+                initialRating: 3,
+                minRating: 1,
+                itemCount: 5,
+                direction: Axis.horizontal,
+                itemSize: 16,
+                itemPadding: EdgeInsets.symmetric(horizontal: 0),
+                itemBuilder: (context, index) {
+                  return Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  );
+                },
+                ignoreGestures: true,
+                updateOnDrag: false,
+                onRatingUpdate: (value) {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future <void> getIdeaInfo() async {
+      // idea 정보들을 가지고 와서 멤버 (전역) 변수 리스트 객체에 담기
   }
 }
